fb3a8e93540107fe27fae229a09ad33646be2f14
jacobgo1 <jacob.golsson@gmail.com>
2026-03-19 00:08:35 +0100

    i mainen var det en del magic nubers så fikse topp i den

--- main.go
+++ main.go
@@ -1,87 +1,68 @@
- package main
-
+package main
 
 import (
 	"fmt"
-	"os"
 	"log"
+	"os"
 
-	"heis/worldview"
 	"heis/config"
 	"heis/distributor"
 	"heis/driver/elevio"
 	"heis/elevtype"
-	"heis/network/peers"
 	"heis/fsm"
+	"heis/network/peers"
+	"heis/worldview"
+)
+
+const (
+	stateChannelBufferSize    = 4
+	finishedRequestBufferSize = 8
 )
 
 func main() {
-	
 	if len(os.Args) < 2 {
 		log.Fatal("Usage: ./heis <config-file>  e.g.  ./heis config/elevator1.con")
 	}
+
 	conf := config.ReadConfigFile(os.Args[1])
 	id := conf.Id
 	fmt.Printf("[main] Starting elevator  id=%q  port=%d\n", id, conf.PortIo)
 
-
-	
-
 	elevio.Init(fmt.Sprintf("localhost:%d", conf.PortIo), config.N_FLOORS)
 
-
-
-	fmt.Printf("[main] Starting elevator node  id=%q\n", id)
-
-
-	peerUpdateCh  := make(chan peers.PeerUpdate)
+	peerUpdateCh := make(chan peers.PeerUpdate)
 	transmitEnable := make(chan bool, 1)
 
 	go peers.Transmitter(config.PortPeers, id, transmitEnable)
 	go peers.Receiver(config.PortPeers, peerUpdateCh)
-
-	
 	transmitEnable <- true
 
-	
-	stateMsgCh := make(chan fsm.ElevatorStateMsg, 4)
-	stateCh    := make(chan worldview.ElevatorState, 4)
-
-	
+	stateMsgCh := make(chan fsm.ElevatorStateMsg, stateChannelBufferSize)
+	stateCh := make(chan worldview.ElevatorState, stateChannelBufferSize)
 	assignmentCh := make(chan [config.N_FLOORS][config.N_BUTTONS]bool, 1)
-
-	
-	finReqCh := make(chan elevio.ButtonEvent, 8)
-
-	
+	finReqCh := make(chan elevio.ButtonEvent, finishedRequestBufferSize)
 	floorCh := make(chan int)
 	obstrCh := make(chan bool)
 
-	
 	go elevio.PollFloorSensor(floorCh)
 	go elevio.PollObstructionSwitch(obstrCh)
-	
-	go func() {
-		for msg := range stateMsgCh {
-			stateCh <- worldview.ElevatorState{
-				Floor:     msg.Floor,
-				Direction: elevtype.Dirn(msg.Dirn),
-				Behaviour: fsmStateToBehaviour(msg.State),
-			}
-		}
-	}()
+	go translateFSMStateToWorldView(stateMsgCh, stateCh)
 
-	
 	io := fsm.NewIO(elevioAdapter{})
 
-	
 	go distributor.Distributor(id, stateCh, finReqCh, assignmentCh, peerUpdateCh)
-
-	
 	fsm.RunFSM(io, assignmentCh, finReqCh, stateMsgCh, floorCh, obstrCh)
 }
 
-
+func translateFSMStateToWorldView(in <-chan fsm.ElevatorStateMsg, out chan<- worldview.ElevatorState) {
+	for msg := range in {
+		out <- worldview.ElevatorState{
+			Floor:     msg.Floor,
+			Direction: elevtype.Dirn(msg.Dirn),
+			Behaviour: fsmStateToBehaviour(msg.State),
+		}
+	}
+}
 
 func fsmStateToBehaviour(s fsm.ElevatorState) elevtype.Behaviour {
 	switch s {
@@ -89,7 +70,7 @@ func fsmStateToBehaviour(s fsm.ElevatorState) elevtype.Behaviour {
 		return elevtype.B_Moving
 	case fsm.ES_DoorOpen:
 		return elevtype.B_DoorOpen
-	default: 
+	default:
 		return elevtype.B_Idle
 	}
 }
