df42d50f39a991e145273d7eb16f15af6dd0d9f7
JonasFroiland <jonassfr@stud.ntnu.no>
2026-03-19 08:06:26 +0100

    heh

--- fsm/fsm.go
+++ fsm/fsm.go
@@ -54,12 +54,7 @@ func RunFSM(
 					// nothing to do
 				}
 			case ES_Moving:
-				 pair := ChooseDirection(e)
-    			if pair.state == ES_Idle {
-        			io.SetMotorDirection(D_Stop)
-        			motorTimer.Stop()
-        			e.dirn = D_Stop
-        			e.state = ES_Idle
+
     }
 
 			case ES_DoorOpen:
