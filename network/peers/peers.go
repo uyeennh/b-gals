a68683ab6daf8bf0f342d981cb169c3c2db0a05b
Jens Tjomsland <jenstj@stud.ntnu.no>
2026-03-19 09:55:20 +0100

    newupdatedistribution

--- network/peers/peers.go
+++ network/peers/peers.go
@@ -15,7 +15,7 @@ type PeerUpdate struct {
 }
 
 const interval = 15 * time.Millisecond
-const timeout = 500 * time.Millisecond
+const timeout = 300 * time.Millisecond
 
 func Transmitter(port int, id string, transmitEnable <-chan bool) {
 
